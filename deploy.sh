e5568d5991ea4da18f9c7fb7392936c5efac5341
Kyle D. Ross <6475128+kyledross@users.noreply.github.com>
2026-05-08 15:35:15 -0400

    Tested and updated with wider selection of Gnome versions.

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 NAME=MaximizeWindowIntoNewWorkspace
 UUID=MaximizeWindowIntoNewWorkspace@kyleross.com
+mkdir -p ~/.local/share/gnome-shell/extensions
 rm -rf ~/.local/share/gnome-shell/extensions/$UUID
 cp -r $NAME ~/.local/share/gnome-shell/extensions/$UUID
